69bdb0063c2d34de7ea267ce9f297a375246c880
Chris <cgkelly@ucdavis.edu>
2019-03-14 14:48:57 -0700

    heroku ports and start scripts

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
+var port = process.env.PORT || 3000;
 var app = require('express')();
 
 app.get("/", function(req, res){
     res.send("<h1>Hello World</h1>");
 });
 
-app.listen(process.env.PORT || 3000, function(){
+app.listen(port, function(){
     console.log("Server has launched on ");
 });
