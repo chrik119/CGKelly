8bb7d5a41db85bf33cfa58c7278680c59c341c62
Chris <cgkelly@ucdavis.edu>
2019-03-14 12:12:47 -0700

    potential port fix

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ app.get("/", function(req, res){
     res.send("<h1>Hello World</h1>");
 });
 
-app.listen(3000, function(){
-    console.log("Server has launched on " + port);
+app.listen(5000, function(){
+    console.log("Server has launched on ");
 });
