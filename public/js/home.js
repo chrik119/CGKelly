a1cb76122822f6ba14bc2ee2dc37517fee0fd413
Chris Kelly <cgkelly@ucdavis.edu>
2019-03-18 14:23:56 -0700

    Collapsable navbar

---
+++ public/js/home.js
@@ -0,0 +1,5 @@
+var colMenu = document.getElementById('colMenu');
+var colButton = document.getElementById('colButton');
+colButton.addEventListener('click', function(){
+    colMenu.classList.toggle('hidden');
+});
